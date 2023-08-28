8711e7935047ebda2794ea5254a4a6bf2ffb1aeb
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-08-28 03:27:05 +0100

    create a GIThub user javascript

---
+++ js/GithubUser.js
@@ -0,0 +1,20 @@
+// Create a class and export
+export class GithubUser {
+
+  static(username) {
+
+    // Create a const end point and pass the api link
+    const endPoint = `https://api.github.com/users/${username}`
+
+    //return a promise that brings the values and convert to JSON.
+    return fetch(endPoint)
+      .then(data => data.json())
+      .then(({ login, name, public_repos, followers, html_url }) => ({
+        login,
+        name,
+        public_repos,
+        followers,
+        html_url
+      }))
+  }
+}
